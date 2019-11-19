d5ebbfdf690549eca82a1715fb092a70bda3ca9f
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-19 14:27:25 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace __11._19_Student_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.11.19-Student_Exercises
