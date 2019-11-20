6ff3468f687cf93f809efdb18d515330b027525f
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-19 21:56:32 -0600

    Slightly cleaned code

--- Cohort.cs
+++ Cohort.cs
@@ -5,19 +5,15 @@ namespace __11._19_Student_Exercises
 {
     public class Cohort
     {
-        /*
-            You must define a type for representing a cohort in code.
-            The cohort's name (Evening Cohort 6, Day Cohort 25, etc.)
-            The collection of students in the cohort.
-            The collection of instructors in the cohort.
-        */
+        // You must define a type for representing a cohort in code
         public string Name {get;}
+        // The collection of students in the cohort
         public List<Student> Students = new List<Student>();
+        // The collection of instructors in the cohort
         public List<Instructor> Instructors = new List<Instructor>();
         public Cohort( string name )
         {
             Name = name;
         }
-
     }
 }
